80a76b2eb5723f136dd03da68de7e1025052b79c
Hoa Tran <Hoa.Thi.Mai.Tran@external.mgm-tp.com>
2018-12-06 17:09:00 +0700

    TNT-1 Hello wolrd application TNT-23 Init backend project
    Change-Id: Ie17dc423875699423006759a2ee43fdbea22ccee

---
+++ TNTBackEnd/src/main/java/com/mgmtp/internship/tntbe/TntBeApplication.java
@@ -0,0 +1,12 @@
+package com.mgmtp.internship.tntbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TntBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TntBeApplication.class, args);
+    }
+}
