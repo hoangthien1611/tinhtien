12b49c793849175221e1dbc21ee657f9e595f0d9
Linh Ngo <Linh.The.Ngo@external.mgm-tp.com>
2018-12-07 17:00:22 +0700

    TNT-1 Hello world application TNT-25 [Front-end] Display number from Back-end after each request
    Change-Id: If90d4b287c75838148ce04ff55f0b673700d3667

--- be-tinhtien/src/main/java/com/mgmtp/internship/tntbe/controllers/HelloWorldController.java
+++ be-tinhtien/src/main/java/com/mgmtp/internship/tntbe/controllers/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.mgmtp.internship.tntbe.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ public class HelloWorldController {
 
     private Integer beginNumber = 0;
 
+    @CrossOrigin
     @GetMapping("/get-increasing-number")
     public Integer getIncreasingNumber() {
         return ++beginNumber;
--- fe-tinhtien/src/App.tsx
+++ fe-tinhtien/src/App.tsx
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-class App extends Component {
+const BASE_URL = "http://localhost:8080/get-increasing-number";
+
+class App extends Component<{}, {displayNumber?: number}> {
+  state = {
+    displayNumber: 0
+  }
+
+  async componentDidMount(): Promise<void> {
+    const rawResult = await fetch(BASE_URL);
+    const numberResult = (await rawResult.json()) as number;
+    this.setState({
+      displayNumber: numberResult
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <label>Hello, number { this.state.displayNumber }</label>
     );
   }
 }
