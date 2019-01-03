7dd94103ce536b5438d8124fa75a296452efdae2
dtacao.ext <Dao.Thi.Anh.Cao@external.mgm-tp.com>
2019-01-03 10:53:59 +0700

    TNT-91 Edit Person TNT-103 Edit Person (Back-end)
    Change-Id: I89042ff8b39cd06ab16aa9a9997916349df77143

--- be-tinhtien/src/main/java/com/mgmtp/internship/tntbe/controllers/PersonController.java
+++ be-tinhtien/src/main/java/com/mgmtp/internship/tntbe/controllers/PersonController.java
@@ -1,6 +1,7 @@
 package com.mgmtp.internship.tntbe.controllers;
 
 import com.mgmtp.internship.tntbe.dto.PersonDTO;
+import com.mgmtp.internship.tntbe.entities.Person;
 import com.mgmtp.internship.tntbe.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,11 @@ public class PersonController {
     @GetMapping("/{activityUrl}")
     public  Object getPersons(@PathVariable String activityUrl){
         return personService.getPersons(activityUrl);
+    }
+
+    @PutMapping("")
+    public Object updatePerson(@RequestBody Person person){
+        return personService.updatePerson(person);
     }
 
 
--- be-tinhtien/src/main/java/com/mgmtp/internship/tntbe/services/PersonService.java
+++ be-tinhtien/src/main/java/com/mgmtp/internship/tntbe/services/PersonService.java
@@ -9,6 +9,8 @@ import com.mgmtp.internship.tntbe.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PersonService {
 
@@ -20,7 +22,7 @@ public class PersonService {
 
     public Object saveNewPerson(PersonDTO personDTO) {
         Activity activity = activityRepository.findByUrl(personDTO.getActivityUrl());
-        if (activity != null){
+        if (activity != null) {
             return personRepository.save(new Person(personDTO.getName(), true, activity));
         }
         return new ErrorMessage("Activity doesn't exist");
@@ -28,9 +30,23 @@ public class PersonService {
 
     public Object getPersons(String activityUrl) {
         Activity activity = activityRepository.findByUrl(activityUrl);
-        if (activity != null){
+        if (activity != null) {
             return activity.getPersons();
         }
         return new ErrorMessage("Activity doesn't exist");
+    }
+
+    public Object updatePerson(Person person) {
+        if (person != null) {
+            Person oldPerson = personRepository.findById(person.getId()).orElse(null);
+            if (oldPerson != null) {
+                oldPerson.setName(person.getName());
+                return personRepository.save(oldPerson);
+            } else {
+                return new ErrorMessage("Not found Person by id = " + person.getId());
+            }
+        } else {
+            return new ErrorMessage("Null Object Exception");
+        }
     }
 }
