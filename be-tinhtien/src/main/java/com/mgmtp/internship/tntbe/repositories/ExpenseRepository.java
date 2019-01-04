96c860e53cb4e096871be240988339a02e227f3b
vdhphan.ext <Vu.Dang.Hai.Phan@external.mgm-tp.com>
2019-01-04 09:04:49 +0700

    TNT-33 Add expense TNT-119 Fix bugs back-end
    Change-Id: I814c3334e107489cd3b34a4ed202fd7f2d9fb062

--- be-tinhtien/src/main/java/com/mgmtp/internship/tntbe/repositories/ExpenseRepository.java
+++ be-tinhtien/src/main/java/com/mgmtp/internship/tntbe/repositories/ExpenseRepository.java
@@ -1,13 +1,13 @@
 package com.mgmtp.internship.tntbe.repositories;
 
-import com.mgmtp.internship.tntbe.entities.DummyPerson;
 import com.mgmtp.internship.tntbe.entities.Expense;
+import com.mgmtp.internship.tntbe.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ExpenseRepository extends JpaRepository<Expense,Long> {
-    List<Expense> findAllByPersonIn(List<DummyPerson> dummyPersons);
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findAllByPersonInOrderByCreatedDateAsc(List<Person> people);
 }
--- be-tinhtien/src/main/java/com/mgmtp/internship/tntbe/services/ExpenseService.java
+++ be-tinhtien/src/main/java/com/mgmtp/internship/tntbe/services/ExpenseService.java
@@ -4,6 +4,7 @@ import com.mgmtp.internship.tntbe.dto.ExpenseDTO;
 import com.mgmtp.internship.tntbe.entities.Activity;
 import com.mgmtp.internship.tntbe.entities.DummyPerson;
 import com.mgmtp.internship.tntbe.entities.Expense;
+import com.mgmtp.internship.tntbe.entities.Person;
 import com.mgmtp.internship.tntbe.repositories.ActivityRepository;
 import com.mgmtp.internship.tntbe.repositories.DummyPersonRepository;
 import com.mgmtp.internship.tntbe.repositories.ExpenseRepository;
@@ -23,25 +24,18 @@ public class ExpenseService {
     private DummyPersonRepository dummyPersonRepository;
     @Autowired
     private ActivityRepository activityRepository;
-    public Expense saveNewExpense(ExpenseDTO expenseDTO){
-        if(expenseDTO.getName().trim().isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Item name is empty!");
-        }
-
-        else if(expenseDTO.getPersonId() == 0) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"DummyPerson is empty!");
-        }
-
-        else if(expenseDTO.getName().length() >255) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Item name is too long, it must be no larger than 255 characters!");
-        }
-
-        else if(expenseDTO.getAmount() <= 0) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Amount must be larger than 0!");
-        }
-
-        else {
-            //remove extra white space in Item name
+    public Expense saveNewExpense(ExpenseDTO expenseDTO) {
+        if (expenseDTO.getName().trim().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Item name is empty!");
+        } else if (expenseDTO.getPersonId() == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "DummyPerson is empty!");
+        } else if (expenseDTO.getName().length() > 255) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Item name is too long, it must contain less than 255 characters!");
+        } else if (expenseDTO.getAmount() <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Amount must be greater than 0!");
+        } else {
+            // remove extra white space in Item name
             expenseDTO.setName(StringUtils.normalizeSpace(expenseDTO.getName()));
             DummyPerson dummyPerson = dummyPersonRepository.findAllById(expenseDTO.getPersonId());
             return expenseRepository.save(new Expense(dummyPerson, expenseDTO.getName(), expenseDTO.getAmount(), expenseDTO.getCreatedDate()));
@@ -54,7 +48,7 @@ public class ExpenseService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Activity not found!");
         }
 
-        List<DummyPerson> dummyPersons = dummyPersonRepository.findAllByActivity_Id(activity.getId());
-        return expenseRepository.findAllByPersonIn(dummyPersons);
+        List<Person> people = activity.getPersons();
+        return expenseRepository.findAllByPersonInOrderByCreatedDateAsc(people);
     }
 }
