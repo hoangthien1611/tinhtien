6e604ef92e7d0cba4e17b637b9221350ba5fa39f
Hieu Xuan Pham <Hieu.Xuan.Pham@external.mgm-tp.com>
2019-01-07 17:22:37 +0700

    TNT-99 A better welcome page. TNT-139 Fix footer and incorrect spelling.
    Change-Id: Ic808bef1f65d5cf8af237a48e459f5fab427ed4f

--- fe-tinhtien/src/components/main/page.tsx
+++ fe-tinhtien/src/components/main/page.tsx
@@ -57,78 +57,79 @@ export default class MainPage extends React.Component<{}, MainPageState> {
 
   render() {
     const { descriptionData } = this.state;
-    return (
+    return (<>
       <div className="main">
-        <ApplicationHeader
-          leftSlots={[
-            <a href="#">
-              <img src={minorLogo} />
-            </a>,
-            <p>TinhTien</p>
-          ]}
-          rightSlots={<p>Manage team expenses</p>}
-          className="header"
-        />
-        <SizeContainer noGutter>
-          <Row>
-            <div className="top center">
-              <img src={mainLogo} className="center" />
-              <h3 className="text-center text-main">
-                Hey, TinhTien is a shared expense manager for your activity.
-              </h3>
-              <div>
-                {this.state.showForm ? (
-                  <>
-                    <CreateActivityForm />
-                    <Button className="center" onClick={this.hideForm}>
-                      Cancel
+          <ApplicationHeader
+            leftSlots={[
+              <a href="#">
+                <img src={minorLogo} />
+              </a>,
+              <p>TinhTien</p>
+            ]}
+            rightSlots={<p>Manage team expenses</p>}
+            className="header"
+          />
+          <SizeContainer noGutter>
+            <Row>
+              <div className="top center">
+                <img src={mainLogo} className="center" />
+                <h3 className="text-center text-main">
+                  Hey, TinhTien is a shared expense manager for your activity.
+                </h3>
+                <div>
+                  {this.state.showForm ? (
+                    <>
+                      <CreateActivityForm />
+                      <Button className="center" onClick={this.hideForm}>
+                        Cancel
+                      </Button>
+                    </>
+                  ) : (
+                    <Button className="center text-main" onClick={this.showForm}>
+                      <h3>START YOUR ACTIVITY HERE!</h3>
                     </Button>
-                  </>
-                ) : (
-                  <Button className="center text-main" onClick={this.showForm}>
-                    <h3>START YOUR ACTIVITY HERE!</h3>
-                  </Button>
-                )}
+                  )}
+                </div>
               </div>
-            </div>
-          </Row>
-          <Row>
-            <div className="center">
-              <CSSTransition
-                classNames="fade"
-                timeout={1000}
-                in={true}
-                appear={true}
-              >
-                <Description {...descriptionData} />
-              </CSSTransition>
-            </div>
-          </Row>
-          <Row>
-            <ButtonGroup className="center">
-              <Button
-                iconButton
-                icon={<Icon>navigate_before</Icon>}
-                title="Back"
-                secondary
-                onClick={this.prevDescription}
-              />
-              <Button
-                iconButton
-                icon={<Icon>navigate_next</Icon>}
-                title="Next"
-                secondary
-                onClick={this.nextDescription}
-              />
-            </ButtonGroup>
-          </Row>
-        </SizeContainer>
+            </Row>
+            <Row>
+              <div className="center">
+                <CSSTransition
+                  classNames="fade"
+                  timeout={1000}
+                  in={true}
+                  appear={true}
+                >
+                  <Description {...descriptionData} />
+                </CSSTransition>
+              </div>
+            </Row>
+            <Row>
+              <ButtonGroup className="center">
+                <Button
+                  iconButton
+                  icon={<Icon>navigate_before</Icon>}
+                  title="Back"
+                  secondary
+                  onClick={this.prevDescription}
+                />
+                <Button
+                  iconButton
+                  icon={<Icon>navigate_next</Icon>}
+                  title="Next"
+                  secondary
+                  onClick={this.nextDescription}
+                />
+              </ButtonGroup>
+            </Row>
+          </SizeContainer>
+        </div>
         <footer className="footer">
           <address>
             <p>Copyright 2018 © mgm. All rights reversed.</p>
           </address>
         </footer>
-      </div>
+        </>
     );
   }
 }
